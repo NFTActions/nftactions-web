01da0a3546c9d3be037e28919f1e8478757221f0
Jay <jayjson@hey.com>
2022-01-15 15:03:29 -0500

    Added placeholder block

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Header from './components/Header'
+import PlaceholderBlock from './components/PlaceholderBlock'
 
 const App = () => {
   return (
     <div className='container'>
       <Header title='NFTActions' />
+      <PlaceholderBlock text='This is where the chart could be added'/>
+      <PlaceholderBlock text='This is where the table could be added' />
     </div>
   )
 }
