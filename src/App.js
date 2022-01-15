ccc46b912b03e9d0e0e5cf330ccbe07757f5f270
Jay <jayjson@hey.com>
2022-01-15 17:08:04 -0500

    Added a temp bar chart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from './components/Header'
+import BarChart from './components/BarChart'
 import PlaceholderBlock from './components/PlaceholderBlock'
 
 const App = () => {
   return (
     <div className='container'>
       <Header title='NFTActions' />
-      <PlaceholderBlock text='This is where the chart could be added'/>
+      <div><BarChart /></div>      
       <PlaceholderBlock text='This is where the table could be added' />
     </div>
   )
