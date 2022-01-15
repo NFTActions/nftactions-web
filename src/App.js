a143b714faa4e6a782e6ad7637415252a3122464
Jay <jayjson@hey.com>
2022-01-15 15:26:21 -0500

    Created the barchart component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header'
+import BarChart from './components/BarChart'
 import PlaceholderBlock from './components/PlaceholderBlock'
 
 const App = () => {
   return (
     <div className='container'>
       <Header title='NFTActions' />
+      <BarChart />
       <PlaceholderBlock text='This is where the chart could be added'/>
       <PlaceholderBlock text='This is where the table could be added' />
     </div>
---
+++ src/components/BarChart.js
@@ -0,0 +1,9 @@
+const BarChart = () => {
+  return(
+    <div className='container'>
+      <h3>Bar Chart</h3>
+    </div>
+  )
+}
+
+export default BarChart
