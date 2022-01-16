52e7acdeef30db14cd591fce87640e548b6ecd46
Jay <jayjson@hey.com>
2022-01-15 19:36:48 -0500

    Refactored to move the networking logic into App.js for shared use between chart and table

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,6 +1,4 @@
-import React, { useState, useEffect } from 'react'
 import { Bar } from 'react-chartjs-2'
-import axios from 'axios'
 
 // const state = {
 //   labels: ['Bored Ape Yacht Club', 'Cryptopunks', 'Deadfellaz', 'PhantaBear', 'CryptoSkulls', 'World of Women', 'Azuki', 'Mutant Ape Yacht Club', 'Doodles', 'The Sandbox'],
@@ -15,45 +13,7 @@ import axios from 'axios'
 //   ]
 // }
 
-const BarChart = () => {
-  const [chartData, setChartData] = useState({});
-
-  const chart = () => {
-    let salaries = [];
-    let names = [];
-
-    axios
-      .get("http://dummy.restapiexample.com/api/v1/employees")
-      .then(res => {
-        console.log(res)
-        for (const object of res.data.data) {
-          salaries.push(parseInt(object.employee_salary))
-          names.push(object.employee_name)
-        }
-
-        setChartData({
-          labels: names,
-          datasets: [
-            {
-              label: "salaries",
-              data: salaries,
-              backgroundColor: ['rgba(24,104,182,1)'],
-              borderWidth: 2
-            }
-          ]
-        })
-      })
-      .catch(err => {
-        console.log(err);
-      })
-
-    console.log(salaries, names)
-  }
-
-  useEffect(() => {
-    chart()
-  }, [])
-
+const BarChart = ({ chartData }) => {
   return (
     <div>
       <Bar
