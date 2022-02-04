a3c02d62ed0428763428686fe662e7b95602b020
Jin Wang <wangatqueens@gmail.com>
2022-02-04 18:32:19 -0500

    Task-install-prettier
    Installed and ran npx prettier --write ./src

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,52 +1,54 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-const buttonTitle = 'Refresh'
+const buttonTitle = "Refresh";
 
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log('Click')
-    }
+const Header = ({title}) => {
+	const onClick = () => {
+		console.log("Click");
+	};
 
-    return (
-        <div className='container'>
-            <h1 style={headingStyle}>{title}</h1> 
-            <h4 style={descriptionStyle}>The hottest NFT collections on OpenSea right now</h4>
-            <h5 style={detailInfoStyle}>Data loaded &lt; 1 min ago</h5>
-        </div>
-    )
-}
+	return (
+		<div className="container">
+			<h1 style={headingStyle}>{title}</h1>
+			<h4 style={descriptionStyle}>
+				The hottest NFT collections on OpenSea right now
+			</h4>
+			<h5 style={detailInfoStyle}>Data loaded &lt; 1 min ago</h5>
+		</div>
+	);
+};
 
 Header.defaultProps = {
-    title: '<Add title>'
-}
+	title: "<Add title>",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired
-}
+	title: PropTypes.string.isRequired,
+};
 
 const headingStyle = {
-    color: 'black',
-    fontSize: 60,
-    textAlign: 'center',
-    fontWeight: 'bold'
-}
+	color: "black",
+	fontSize: 60,
+	textAlign: "center",
+	fontWeight: "bold",
+};
 
 const descriptionStyle = {
-    color: 'grey',
-    fontSize: 30,
-    textAlign: 'center'
-}
+	color: "grey",
+	fontSize: 30,
+	textAlign: "center",
+};
 
 const detailInfoStyle = {
-    color: 'grey',
-    fontSize: 20,
-    textAlign: 'center',
-    marginTop: 40 
-}
+	color: "grey",
+	fontSize: 20,
+	textAlign: "center",
+	marginTop: 40,
+};
 
 const buttonStyle = {
-    color: 'DodgerBlue',
-    textAlign: 'center'
-}
+	color: "DodgerBlue",
+	textAlign: "center",
+};
 
-export default Header
+export default Header;
--- src/components/PlaceholderBlock.js
+++ src/components/PlaceholderBlock.js
@@ -1,9 +1,9 @@
-const PlaceholderBlock = ({ text }) => {
-    return (
-        <div className='container'>
-          <h3>{text}</h3> 
-        </div>
-    )
-}
+const PlaceholderBlock = ({text}) => {
+	return (
+		<div className="container">
+			<h3>{text}</h3>
+		</div>
+	);
+};
 
-export default PlaceholderBlock
+export default PlaceholderBlock;
