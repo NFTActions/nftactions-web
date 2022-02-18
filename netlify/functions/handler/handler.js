8bc3bb4bdce5c2829184e11b290ebfb01e74386f
Jin Wang <wangatqueens@gmail.com>
2022-02-17 20:14:55 -0500

    Task-Twitter-section
    - Encrypted twitter api token
    - Cleaned up mock data
    - Removed unused file

--- netlify/functions/handler/handler.js
+++ netlify/functions/handler/handler.js
@@ -1,27 +1,45 @@
 const fetch = require("node-fetch");
+const secureEnv = require("secure-env");
 
 const handler = async function () {
 	try {
-		const response = await fetch(
-			`https://api.twitter.com/2/tweets/search/recent?query=nft&tweet.fields=author_id,created_at,entities,geo,in_reply_to_user_id,lang,possibly_sensitive,referenced_tweets,source`,
-			{
-				headers: {
-					Accept: "application/json",
-					//Twitter API token
-					Authorization: `Bearer `,
-				},
-			}
-		);
-		if (!response.ok) {
-			// NOT res.status >= 200 && res.status < 300
-			return {statusCode: response.status, body: response.statusText};
-		}
-		const data = await response.json();
+		if (!process.env.REACT_APP_AFT_ACTION_TWITTER_API_TOKEN_ENV_KEY) {
+			throw new Error(
+				"REACT_APP_AFT_ACTION_TWITTER_API_TOKEN_ENV_KEY has not been set as a variable in your environment."
+			);
+		} else {
+			global.env = secureEnv({
+				path: ".env.twitter-token.enc",
+				secret: process.env.REACT_APP_AFT_ACTION_TWITTER_API_TOKEN_ENV_KEY,
+			});
+			if (!global.env) {
+				throw new Error("Invalid key used for decryption");
+			} else {
+				if (!global.env.REACT_APP_BEARER_TOKEN) {
+					throw new Error("No REACT_APP_BEARER_TOKEN for request tweets");
+				} else {
+					const response = await fetch(
+						`https://api.twitter.com/2/tweets/search/recent?query=nft&tweet.fields=author_id,created_at,entities,geo,in_reply_to_user_id,lang,possibly_sensitive,referenced_tweets,source`,
+						{
+							headers: {
+								Accept: "application/json",
+								Authorization: `Bearer ${global.env.REACT_APP_BEARER_TOKEN}`,
+							},
+						}
+					);
+					if (!response.ok) {
+						// NOT res.status >= 200 && res.status < 300
+						return {statusCode: response.status, body: response.statusText};
+					}
+					const data = await response.json();
 
-		return {
-			statusCode: 200,
-			body: JSON.stringify(data),
-		};
+					return {
+						statusCode: 200,
+						body: JSON.stringify(data),
+					};
+				}
+			}
+		}
 	} catch (error) {
 		// output to netlify function log
 		console.log(error);
--- src/Scripts/Axios/axiosRequests.js
+++ src/Scripts/Axios/axiosRequests.js
@@ -11,20 +11,3 @@ export const getData = async (url) => {
 		return error.response;
 	}
 };
-
-export const getDataWithAuth = async (url) => {
-	try {
-		const response = await axios.get(url, {
-			headers: {
-				//Twitter API token
-				Authorization: "Bearer ",
-			},
-		});
-		if (response.status === 200) {
-			return response.data;
-		}
-	} catch (error) {
-		console.error(error);
-		return error.response;
-	}
-};
